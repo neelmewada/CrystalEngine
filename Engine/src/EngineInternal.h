deaa62a0f185e72d36fb60fbf0294619b1c1b3c5
Neel Mewada <neil@tap-nation.io>
2022-10-17 19:23:03 +0530

    Initial commit
    Signed-off-by: Neel Mewada <neil@tap-nation.io>

--- Engine/src/EngineInternal.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-
-#define VK_ASSERT(result,message) if ((result) != VK_SUCCESS) throw std::runtime_error(message)
--- Engine/src/Rendering/Vulkan/VulkanLimits.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// File containing vulkan and/or rendering related Definitions
-
-#define MAX_BOUND_DESCRIPTOR_SETS 4
-
