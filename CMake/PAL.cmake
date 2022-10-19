9bcb2d551256e650de183d4d92d2874ded9c20b2
Neel Mewada <neil@tap-nation.io>
2022-10-19 19:42:18 +0530

    wip

--- CMake/PAL.cmake
+++ CMake/PAL.cmake
@@ -1,61 +1,63 @@
 
 include_guard(GLOBAL)
 
-if (DEFINED CE_STANDALONE)
+if (CE_STANDALONE)
     
     if (${CE_STANDALONE} STREQUAL "Windows")
 
-        set(CE_STANDALONE_PLATFORM "Windows")
+        set(PAL_STANDALONE_PLATFORM_NAME "Windows")
         set(CMAKE_SYSTEM_NAME "Windows")
         set(CE_STANDALONE ON)
-        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+        set(PAL_PLATFORM_NAME ${PAL_STANDALONE_PLATFORM_NAME})
 
     elseif(${CE_STANDALONE} STREQUAL "Mac")
 
-        set(CE_STANDALONE_PLATFORM "Mac")
+        set(PAL_STANDALONE_PLATFORM_NAME "Mac")
         set(CMAKE_SYSTEM_NAME "Darwin")
         set(CE_STANDALONE ON)
-        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+        set(PAL_PLATFORM_NAME ${PAL_STANDALONE_PLATFORM_NAME})
 
     elseif(${CE_STANDALONE} STREQUAL "Linux")
 
-        set(CE_STANDALONE_PLATFORM "Linux")
+        set(PAL_STANDALONE_PLATFORM_NAME "Linux")
         set(CMAKE_SYSTEM_NAME "Linux")
         set(CE_STANDALONE ON)
-        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+        set(PAL_PLATFORM_NAME ${PAL_STANDALONE_PLATFORM_NAME})
 
     elseif(${CE_STANDALONE} STREQUAL "Android")
 
-        set(CE_STANDALONE_PLATFORM "Android")
+        set(PAL_STANDALONE_PLATFORM_NAME "Android")
         set(CMAKE_SYSTEM_NAME "Android")
         set(CE_STANDALONE ON)
-        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+        set(PAL_PLATFORM_NAME ${PAL_STANDALONE_PLATFORM_NAME})
 
     elseif(${CE_STANDALONE} STREQUAL "iOS")
 
-        set(CE_STANDALONE_PLATFORM "iOS")
+        set(PAL_STANDALONE_PLATFORM_NAME "iOS")
         set(CMAKE_SYSTEM_NAME "iOS")
         set(CE_STANDALONE ON)
-        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+        set(PAL_PLATFORM_NAME ${PAL_STANDALONE_PLATFORM_NAME})
 
     elseif(${CE_STANDALONE} STREQUAL "OFF")
 
         set(CE_STANDALONE OFF)
-        set(CE_PLATFORM ${CMAKE_SYSTEM_NAME})
+        set(PAL_PLATFORM_NAME ${CMAKE_SYSTEM_NAME})
         
     else()
         message(FATAL_ERROR "Invalid CE_STANDALONE platform passed: ${CE_STANDALONE}")
     endif()
 
 else()
-    set(CE_PLATFORM ${CMAKE_SYSTEM_NAME})
+    set(PAL_PLATFORM_NAME ${CMAKE_SYSTEM_NAME})
+    set(PAL_HOST_PLATFORM_NAME ${CMAKE_SYSTEM_NAME})
 endif()
 
 if (${CE_STANDALONE})
-    message(STATUS "Standalone Build: ${CE_PLATFORM}")
+    message(STATUS "Standalone Build: ${PAL_PLATFORM_NAME}")
 else()
-    message(STATUS "Editor Build: ${CE_PLATFORM}")
+    message(STATUS "Editor Build: ${PAL_PLATFORM_NAME}")
 endif()
+
 
 
 
