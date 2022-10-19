ced283cd74cdc5df0c05925ad42eea8c1897c963
Neel Mewada <neelrmewada@gmail.com>
2022-10-19 17:00:55 +0530

    wip

--- CMake/PlatformSettings.cmake
+++ CMake/PlatformSettings.cmake
@@ -1,34 +1,62 @@
 
 include_guard(GLOBAL)
 
-set(CE_HOST_PLATFORM ${CMAKE_SYSTEM_NAME})
+if (DEFINED CE_STANDALONE)
+    
+    if (${CE_STANDALONE} STREQUAL "Windows")
 
-if("${CE_STANDALONE_PLATFORM}" STREQUAL "Mac")
-    set(CE_STANDALONE_PLATFORM "Darwin" CACHE STRING "Target platform for standalone build")
-elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "Windows")
-    set(CE_STANDALONE_PLATFORM "Windows" CACHE STRING "Target platform for standalone build")
-elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "Linux")
-    set(CE_STANDALONE_PLATFORM "Linux" CACHE STRING "Target platform for standalone build")
-elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "Android")
-    set(CE_STANDALONE_PLATFORM "Android" CACHE STRING "Target platform for standalone build")
-elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "iOS")
-    set(CE_STANDALONE_PLATFORM "iOS" CACHE STRING "Target platform for standalone build")
-else()
-    set(CE_STANDALONE_PLATFORM "${CMAKE_SYSTEM_NAME}" CACHE STRING "Target platform for standalone build")
-endif()
+        set(CE_STANDALONE_PLATFORM "Windows")
+        set(CMAKE_SYSTEM_NAME "Windows")
+        set(CE_STANDALONE ON)
+        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
 
-message(STATUS "Host: ${CE_HOST_PLATFORM}")
-message(STATUS "Standalone: ${CE_STANDALONE_PLATFORM}")
+    elseif(${CE_STANDALONE} STREQUAL "Mac")
 
-if("${CE_HOST_PLATFORM}" STREQUAL "Windows")
-    if("${CE_STANDALONE_PLATFORM}" STREQUAL "Mac")
-        message(FATAL_ERROR "ERROR: Can't build for Mac on Windows host")
-    elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "Linux")
-        message(FATAL_ERROR "ERROR: Can't build for Linux on Windows host")
-    elseif("${CE_STANDALONE_PLATFORM}" STREQUAL "iOS")
-        message(FATAL_ERROR "ERROR: Can't build for iOS on Windows host")
+        set(CE_STANDALONE_PLATFORM "Mac")
+        set(CMAKE_SYSTEM_NAME "Darwin")
+        set(CE_STANDALONE ON)
+        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+
+    elseif(${CE_STANDALONE} STREQUAL "Linux")
+
+        set(CE_STANDALONE_PLATFORM "Linux")
+        set(CMAKE_SYSTEM_NAME "Linux")
+        set(CE_STANDALONE ON)
+        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+
+    elseif(${CE_STANDALONE} STREQUAL "Android")
+
+        set(CE_STANDALONE_PLATFORM "Android")
+        set(CMAKE_SYSTEM_NAME "Android")
+        set(CE_STANDALONE ON)
+        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+
+    elseif(${CE_STANDALONE} STREQUAL "iOS")
+
+        set(CE_STANDALONE_PLATFORM "iOS")
+        set(CMAKE_SYSTEM_NAME "iOS")
+        set(CE_STANDALONE ON)
+        set(CE_PLATFORM ${CE_STANDALONE_PLATFORM})
+
+    elseif(${CE_STANDALONE} STREQUAL "OFF")
+
+        set(CE_STANDALONE OFF)
+        set(CE_PLATFORM ${CMAKE_SYSTEM_NAME})
+        
+    else()
+        message(FATAL_ERROR "Invalid CE_STANDALONE platform passed: ${CE_STANDALONE}")
     endif()
+
+else()
+    set(CE_PLATFORM ${CMAKE_SYSTEM_NAME})
 endif()
+
+if (${CE_STANDALONE})
+    message(STATUS "Standalone Build: ${CE_PLATFORM}")
+else()
+    message(STATUS "Editor Build: ${CE_PLATFORM}")
+endif()
+
 
 
 
