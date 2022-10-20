1ba15f87f24bb0918d0c47786b0788894d9ae1c7
Neel Mewada <neelrmewada@gmail.com>
2022-10-20 17:55:05 +0530

    wip

--- CMake/OutputDirectory.cmake
+++ CMake/OutputDirectory.cmake
@@ -1,8 +1,10 @@
 
-# Set output directories
-set(CE_BINARY_DIR ${CMAKE_SOURCE_DIR}/Binaries/)
+set(CE_OUTPUT_DIR ${CMAKE_BINARY_DIR}/$<CONFIG>/)
 
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/Libraries CACHE PATH "Build directory for static libraries and import libraries")
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/Binaries CACHE PATH "Build directory for shared libraries")
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/Binaries CACHE PATH "Build directory for executables")
+# Set default output directories
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CE_OUTPUT_DIR}/Libraries CACHE PATH "Build directory for static libraries and import libraries")
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CE_OUTPUT_DIR}/ CACHE PATH "Build directory for shared libraries")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CE_OUTPUT_DIR}/ CACHE PATH "Build directory for executables")
+
+set(CE_STANDALONE_BINARY_DIR ${CE_OUTPUT_DIR}/Standalone/${PAL_PLATFORM_NAME}/)
 
