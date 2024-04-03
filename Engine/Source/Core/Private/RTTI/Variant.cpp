83c64359edfcd9feed39bccb52b4a48514b16516
Neel <neelrmewada@gmail.com>
2024-04-03 10:33:11 +0530

    Variant memory leak fix

--- Engine/Source/Core/Private/RTTI/Variant.cpp
+++ Engine/Source/Core/Private/RTTI/Variant.cpp
@@ -5,17 +5,74 @@ namespace CE
 
 	Variant::~Variant()
 	{
-		
+		Free();
 	}
 
 	void Variant::CopyFrom(const Variant& copy)
 	{
-		memcpy(this, &copy, sizeof(Variant));
+		valueTypeId = copy.valueTypeId;
+		arrayElementTypeId = copy.arrayElementTypeId;
+		isPointer = copy.isPointer;
+		isArray = copy.isArray;
+
+		if (copy.IsOfType<String>())
+		{
+			StringValue = copy.StringValue;
+		}
+		else if (copy.isArray && copy.arrayElementTypeId == TYPEID(String))
+		{
+			stringArrayValue = copy.stringArrayValue;
+		}
+		else if (copy.isArray && copy.arrayElementTypeId == TYPEID(Name))
+		{
+			nameArrayValue = copy.nameArrayValue;
+		}
+		else if (copy.isArray && copy.arrayElementTypeId == TYPEID(IO::Path))
+		{
+			pathArrayValue = copy.pathArrayValue;
+		}
+		else if (copy.isArray)
+		{
+			rawArrayValue = copy.rawArrayValue;
+		}
+		else if (copy.IsOfType<IO::Path>())
+		{
+			pathValue = copy.pathValue;
+		}
+		else
+		{
+			memcpy(this, &copy, sizeof(Variant));
+		}
 	}
 
 	void Variant::Free()
 	{
-		
+		if (IsOfType<String>())
+		{
+			StringValue.~String();
+		}
+		else if (isArray && arrayElementTypeId == TYPEID(String))
+		{
+			stringArrayValue.~Array();
+		}
+		else if (isArray && arrayElementTypeId == TYPEID(Name))
+		{
+			nameArrayValue.~Array();
+		}
+		else if (isArray && arrayElementTypeId == TYPEID(IO::Path))
+		{
+			pathArrayValue.~Array();
+		}
+		else if (isArray)
+		{
+			rawArrayValue.~Array();
+		}
+		else if (IsOfType<IO::Path>())
+		{
+			pathValue.~Path();
+		}
+
+		memset(this, 0, sizeof(Variant));
 	}
     
 } // namespace CE
