487943ba57953150ae3fc243f232afabcd215643
Neel <neelrmewada@gmail.com>
2024-05-12 13:35:45 +0530

    sync

--- Engine/Source/CrystalWidgets/Public/CrystalWidgets/Validation/CTextInputValidation.h
+++ Engine/Source/CrystalWidgets/Public/CrystalWidgets/Validation/CTextInputValidation.h
@@ -6,6 +6,16 @@ namespace CE::Widgets
     typedef Delegate<bool(const String& text)> CInputValidator;
 
     CRYSTALWIDGETS_API bool CFloatInputValidator(const String& text);
+
+    CRYSTALWIDGETS_API bool CIntegerInputValidator(const String& text);
+
+    CRYSTALWIDGETS_API bool CSignedIntegerInputValidator(const String& text);
+
+    CRYSTALWIDGETS_API bool CObjectNameInputValidator(const String& text);
+
+    CRYSTALWIDGETS_API bool CPackageNameInputValidator(const String& text);
+
+    CRYSTALWIDGETS_API bool CTypeNameInputValidator(const String& text);
     
 } // namespace CE::Widgets
 
