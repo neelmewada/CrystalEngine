6d2481908c1392eadf727e1415109effdda935bf
Neil Mewada <neelrmewada@gmail.com>
2024-12-05 17:16:37 +0530

    wip

--- Engine/Source/FusionCore/Private/Widget/FContainerWidget.cpp
+++ Engine/Source/FusionCore/Private/Widget/FContainerWidget.cpp
@@ -30,7 +30,7 @@ namespace CE
         Vec3 invScale = Vec3(1 / m_Scale.x, 1 / m_Scale.y, 1);
 
         Vec2 transformedMousePos = (Matrix4x4::Translation(-computedPosition - m_Translation) *
-            Matrix4x4::Angle(-m_Angle) *
+            Matrix4x4::Angle(m_Angle) *
             Matrix4x4::Scale(invScale)) *
             Vec4(localMousePos.x, localMousePos.y, 0, 1);
         
@@ -39,6 +39,11 @@ namespace CE
             Ref<FWidget> child = children[i].Get();
             if (!child->Enabled())
                 continue;
+
+            /*Matrix4x4::Translation(GetComputedPosition() + Translation() + GetComputedSize() / 2)*
+                Matrix4x4::Angle(Angle())*
+                Matrix4x4::Translation(-GetComputedSize() / 2)*
+                Matrix4x4::Scale(Scale());*/
 
             FWidget* result = child->HitTest(transformedMousePos);
             if (result)
